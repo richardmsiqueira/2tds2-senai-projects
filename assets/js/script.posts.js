0da3be7e15f596dcb94ddc467dbe9585335eee9d
Richard Macedo <richardmacedo.564@gmail.com>
2023-08-08 14:31:24 -0300

    feat: create store post function

--- assets/js/script.posts.js
+++ assets/js/script.posts.js
@@ -1,3 +1,5 @@
+const posts = [];
+
 function savePost () {
     const title = document.getElementById("title").value;
     const category = document.getElementById("category").value;
@@ -5,9 +7,15 @@ function savePost () {
     const author = document.getElementById("author").value;
     const date = document.getElementById("date").value;
 
-    console.log(title, category, resume, author, date, )
+    if(title && category && resume && author && date) {
+        storePost(title, category, resume, author, date);
+    } else {
+        alert("Preencha todos os campos!")
+    }
 
-    cleanFields()
+    storePost(title, category, resume, author, date);
+
+    cleanFields();
 }
 
 function cleanFields () {
@@ -17,3 +25,16 @@ function cleanFields () {
     document.getElementById("author").value = "";
     document.getElementById("date").value = "";
 }  
+
+function storePost(title, category, resume, author, date) {
+    const post = {
+        title,
+        category,
+        resume,
+        author,
+        date
+    };
+    posts.push(post);
+
+    console.log(posts);
+}
