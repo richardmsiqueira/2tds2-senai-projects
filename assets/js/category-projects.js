c265f25c2674c192972026cad9efe06a3963cf32
Richard Macedo <richardmacedo.564@gmail.com>
2023-08-17 10:18:02 -0300

    feat: add class Product

--- assets/js/category-projects.js
+++ assets/js/category-projects.js
@@ -6,3 +6,13 @@ class Category {
         this.products = [];
     }
 }
+
+//produto pertence a uma categoria => 1:1
+class Product {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name; 
+        this.price = price;
+        this.category = category;
+    }
+}
