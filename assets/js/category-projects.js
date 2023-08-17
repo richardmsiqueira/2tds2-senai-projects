27449a6970ddacafdcf68ffaaf3d87d11460aee9
Richard Macedo <richardmacedo.564@gmail.com>
2023-08-17 10:31:41 -0300

    feat: create addCategory method

--- assets/js/category-projects.js
+++ assets/js/category-projects.js
@@ -16,3 +16,16 @@ class Product {
         this.category = category;
     }
 }
+
+class CategoryService {
+    constructor() {
+        this.categories = [];
+        this.nextCategoryId = 0;
+    } 
+
+    addCategory(name) {
+        const id = this.nextCategoryId++;
+        const category = new Category(id, name);
+        this.categories.push(category)
+    }
+}
